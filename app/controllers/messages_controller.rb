3b16015318321c84777ff75e91e0b075b7c3e8bc
Ruan Brandão <ruan.bernardo@gmail.com>
2016-05-23 22:38:45 -0300

    Fill actions for Messages Controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :destroy]
+
   def index
     @messages = Message.all
   end
@@ -7,11 +9,32 @@ class MessagesController < ApplicationController
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+
+    if @message.save
+      redirect_to @message
+    else
+      render :new
+    end
   end
 
   def destroy
+    @message.destroy
+    redirect_to messages_path
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :user)
   end
 end
